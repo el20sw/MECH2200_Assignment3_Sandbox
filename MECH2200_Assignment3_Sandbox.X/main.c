b53e5becd6fe0d0b8446a5212dc46fb764214c1e
Sam Wilson <91544749+el20sw@users.noreply.github.com>
2022-03-15 15:37:00 +0000

    Initial Upload

---
+++ MECH2200_Assignment3_Sandbox.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: samwi
+ *
+ * Created on 15 March 2022, 15:19
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    
+    unsigned int Val;
+    
+    Val = 2;
+    
+    return;
+}
---
+++ MECH2200_Assignment3_Sandbox.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MECH2200_Assignment3_Sandbox.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MECH2200_Assignment3_Sandbox.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mech2200assignment3sandbox.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mech2200assignment3sandbox.x.tar
